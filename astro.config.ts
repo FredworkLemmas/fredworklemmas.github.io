2f40d598d940a5a1249b0d80dbf37d2b1dffa01d
Fred McDavid <fred@rejoiner.com>
2025-08-01 00:03:03 -0400

    updates site config

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,8 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
-  site: 'https://astro-theme-vitesse.netlify.app/',
+  site: 'https://fredworklemmas.github.io/',
+  base: 'fredworklemmas',
   server: {
     port: 1977,
   },
